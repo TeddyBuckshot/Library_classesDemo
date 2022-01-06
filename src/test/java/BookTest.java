10a2d83661855feeda2c9efe6af4e1560d0a18a6
Sam <samspencer2202@outlook.com>
2022-01-06 04:17:57 +0000

    bookTests added

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    Book book;
+
+    @Before
+    public void setUp(){
+        book = new Book("Lord of the Rings", "J. R. R. Tolkien", "High Fantasy");
+    }
+
+    @Test
+    public void hasTitle(){
+        assertEquals("Lord of the Rings", book.getTitle());
+    }
+    @Test
+    public void hasAuthor(){
+        assertEquals("J. R. R. Tolkien", book.getAuthor());
+    }
+    @Test
+    public void hasGenre(){
+        assertEquals("High Fantasy", book.getGenre());
+    }
 }
